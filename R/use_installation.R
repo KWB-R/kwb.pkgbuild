d9123b5841d120d380bc4352a14b64802f3f25b4
Hauke Sonnenberg <hsonne@users.noreply.github.com>
2018-09-27 16:36:17 +0200

    Fix bug: missing double quote
    and use if () instead of ifelse()

--- R/use_installation.R
+++ R/use_installation.R
@@ -5,18 +5,16 @@
 #' @param domain under which repository is hosted
 #' @param output  if "rmd" (other code chunks are used); otherwise: "md" style!
 #' (default: "md")
-use_installation <- function(pkgname, user, domain, output = "md") {
-
+use_installation <- function(pkgname, user, domain, output = "md")
+{
   c("# Installation",
     "",
-    ifelse(tolower(output)=="rmd",
-           "```{r echo = TRUE, eval = FALSE}",
-           "```r"),
+    if (tolower(output) == "rmd") {
+      "```{r echo = TRUE, eval = FALSE}"
+    } else {
+      "```r"
+    },
     "#install.packages(\"devtools\", repos = \"https://cloud.r-project.org\")",
-    sprintf("devtools::install_%s(\"%s/%s)",
-            domain,
-            user,
-            pkgname),
+    sprintf("devtools::install_%s(\"%s/%s\")", domain, user, pkgname),
     "```")
-
 }
