baf23ae44adabc6fe3924fcc105e832bd7b331f2
mrustl <michael.rustler@kompetenz-wasser.de>
2019-01-09 13:18:56 +0100

    set commit name for "gh-pages" creation to
    kwb.pkgbuild::use_autopkgdown as it is automatically created with this function and also remove ".gitlab-ci.yml" from master branch (if existing). "docs" folder still needs to be deleted manually as it might be copied to the "gh-pages" branch (due to an exisiting "release" docu that should be kept!)

--- R/use_autopkgdown.R
+++ R/use_autopkgdown.R
@@ -50,6 +50,12 @@ use_autopkgdown <- function(repo = NULL,
   ### 4) Create .gitlab-ci.yml for "master" branch with "docs" folder
   use_gitlab_ci_docs(dest_dir = file.path(dest_dir, repo))
 
-  # 4) Update .travis.yml
+  # 5) Update .travis.yml
   use_travis(auto_build_pkgdown = TRUE, dbg)
+
+  # 6) Delete .gitlab-ci.yml (if existing in "master" branch)
+  fs::file_delete(".gitlab-ci.yml")
+
+#  # 7) Delete "docs" folder (if existing in "master" branch)
+#  fs::dir_delete(path = "docs")
 }
