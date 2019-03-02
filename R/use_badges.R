b9023f695e786f5d5c43fc41d9597a7fe946d04b
hsonne <hauke.sonnenberg@gmx.de>
2019-03-03 00:11:31 +0100

    Clean use_badges.R
    - use sapply() in use_badge_cran()

--- R/use_badges.R
+++ R/use_badges.R
@@ -1,3 +1,5 @@
+# use_badge_appveyor -----------------------------------------------------------
+
 #' Badge appveyor
 #' @param repo name of repository (default: NULL)
 #' @param user user name or organisation under which repository defined in
@@ -6,7 +8,8 @@
 #'
 #' @return generates appveyor badge link
 #' @export
-use_badge_appveyor <- function(repo = NULL, user = "KWB-R", domain = "github") {
+use_badge_appveyor <- function(repo = NULL, user = "KWB-R", domain = "github")
+{
   pkgname <- get_pkgname(repo)
 
   sprintf(
@@ -16,13 +19,11 @@ use_badge_appveyor <- function(repo = NULL, user = "KWB-R", domain = "github") {
       "%s/%s?branch=master&svg=true)]",
       "(https://ci.appveyor.com/project/%s/%s/branch/master)"
     ),
-    domain,
-    user,
-    pkgname,
-    user,
-    gsub(pattern = "\\.", replacement = "-", pkgname)
+    domain, user, pkgname, user, gsub("\\.", "-", pkgname)
   )
 }
+
+# use_badge_travis -------------------------------------------------------------
 
 #' Badge travis
 #' @param repo name of repository (default: NULL)
@@ -30,7 +31,8 @@ use_badge_appveyor <- function(repo = NULL, user = "KWB-R", domain = "github") {
 #' parameter "repo" is hosted (default: KWB-R")
 #' @return generates travis badge link
 #' @export
-use_badge_travis <- function(repo = NULL, user = "KWB-R") {
+use_badge_travis <- function(repo = NULL, user = "KWB-R")
+{
   pkgname <- get_pkgname(repo)
 
   sprintf(
@@ -38,12 +40,11 @@ use_badge_travis <- function(repo = NULL, user = "KWB-R") {
       "[![Travis build Status](https://travis-ci.org/",
       "%s/%s.svg?branch=master)](https://travis-ci.org/%s/%s)"
     ),
-    user,
-    pkgname,
-    user,
-    pkgname
+    user, pkgname, user, pkgname
   )
 }
+
+# use_badge_codecov ------------------------------------------------------------
 
 #' Badge codecov
 #' @param repo name of repository (default: NULL)
@@ -52,7 +53,8 @@ use_badge_travis <- function(repo = NULL, user = "KWB-R") {
 #' @param domain under which repository is hosted (default: "github")
 #' @return generates codecov badge link
 #' @export
-use_badge_codecov <- function(repo = NULL, user = "KWB-R", domain = "github") {
+use_badge_codecov <- function(repo = NULL, user = "KWB-R", domain = "github")
+{
   pkgname <- get_pkgname(repo)
 
   sprintf(
@@ -60,23 +62,22 @@ use_badge_codecov <- function(repo = NULL, user = "KWB-R", domain = "github") {
       "[![codecov](https://codecov.io/%s/%s/%s/branch/",
       "master/graphs/badge.svg)](https://codecov.io/%s/%s/%s)"
     ),
-    domain,
-    user,
-    pkgname,
-    domain,
-    user,
-    pkgname
+    domain, user, pkgname, domain, user, pkgname
   )
 }
 
+# use_badge_lifecycle ----------------------------------------------------------
+
 #' Badge lifecycle
 #' @param stage badge declares the developmental stage of a package, according
-#' to [https://www.tidyverse.org/lifecycle/](https://www.tidyverse.org/lifecycle/),
-#' valid arguments are: "experimental", "maturing", "stable", "retired",
-#' "archived", "dormant", "questioning"), (default: "experiment")
+#'   to
+#'   [https://www.tidyverse.org/lifecycle/](https://www.tidyverse.org/lifecycle/),
+#'    valid arguments are: "experimental", "maturing", "stable", "retired",
+#'   "archived", "dormant", "questioning"), (default: "experiment")
 #' @return generates lifecycle badge link
 #' @export
-use_badge_lifecycle <- function(stage = "experimental") {
+use_badge_lifecycle <- function(stage = "experimental")
+{
   stages <- usethis:::stages
   stage <- match.arg(tolower(stage), names(stages))
   colour <- stages[[stage]]
@@ -88,11 +89,15 @@ use_badge_lifecycle <- function(stage = "experimental") {
   sprintf("[![Project Status](%s)](%s)", src, href)
 }
 
+# is_on_cran -------------------------------------------------------------------
+
 #' Helper function for checking if docu on CRAN
 #' @keywords internal
 #' @noRd
-is_on_cran <- function(cran_link) {
-  try(httr::status_code(x = httr::GET(cran_link)) == 200,silent = TRUE)
+is_on_cran <- function(cran_link)
+{
+  try(httr::status_code(x = httr::GET(cran_link)) == 200, silent = TRUE)
+
   # tryCatch(httr::status_code(x = httr::GET(cran_link)) == 200,
   #   error = function(e) {
   #     print("caught error")
@@ -101,9 +106,9 @@ is_on_cran <- function(cran_link) {
   #       cran_link, e
   #     ))
   #     "error"})
-  }
+}
 
-
+# use_badge_cran ---------------------------------------------------------------
 
 #' Badge CRAN
 #' @param pkgname name of R package (default: NULL)
@@ -112,8 +117,8 @@ is_on_cran <- function(cran_link) {
 #' @importFrom assertthat is.error
 #' @importFrom kwb.utils defaultIfNULL
 #' @export
-use_badge_cran <- function(pkgname = NULL) {
-
+use_badge_cran <- function(pkgname = NULL)
+{
   pkgname <- kwb.utils::defaultIfNULL(pkgname, get_pkgname(pkgname))
 
   cran_mirrors <- sprintf("https://cran.%s.org", c("r-project", "rstudio"))
@@ -122,33 +127,32 @@ use_badge_cran <- function(pkgname = NULL) {
   res_link1 <- is_on_cran(cran_mirrors_link[1])
   res_link2 <- is_on_cran(cran_mirrors_link[2])
 
-  is_no_error <- c(assertthat::is.error(res_link1),
-                   assertthat::is.error(res_link2))
+  is_no_error <- sapply(c(res_link1, res_link2), assertthat::is.error)
 
-  pkg_on_cran <- tryCatch(ifelse(! assertthat::is.error(res_link1),
-                                 res_link1,
-                                 res_link2),
-  error = function(e) {
-    print("caught error")
-    cat(sprintf(
-      "Requesting %s and %s failed with an error:\n%s",
-      res_link1,
-      res_link2,
-      e
-    ))
-  }
+  pkg_on_cran <- tryCatch(
+    ifelse(! assertthat::is.error(res_link1), res_link1, res_link2),
+    error = function(e) {
+      print("caught error")
+      cat(sprintf(
+        "Requesting %s and %s failed with an error:\n%s",
+        res_link1,
+        res_link2,
+        e
+      ))
+    }
   )
 
   href <- "()"
-  if (pkg_on_cran) href <- sprintf("(%s)", cran_mirrors_link[is_no_error])
 
+  if (pkg_on_cran) {
+    href <- sprintf("(%s)", cran_mirrors_link[is_no_error])
+  }
 
   sprintf(
     paste0(
       "[![CRAN_Status_Badge](https://www.r-pkg.org/badges/version/",
       "%s)]%s"
     ),
-    pkgname,
-    href
+    pkgname, href
   )
 }
