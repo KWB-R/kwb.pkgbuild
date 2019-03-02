f617d513f946c5a7e9fc899dfb9e0e0da516e576
hsonne <hauke.sonnenberg@gmx.de>
2019-03-03 00:15:56 +0100

    Clean use_codecov.R
    - no need for variable "new"

--- R/use_codecov.R
+++ R/use_codecov.R
@@ -1,14 +1,15 @@
 # use_codecov ----------------------------------------------------------------
+
 #' Adds codecov.yml
 #' @return writes codecov.yml and adds it .Rbuildignore
 #' @importFrom usethis use_template
 #' @export
-use_codecov <- function() {
-
+use_codecov <- function()
+{
   usethis:::check_uses_travis()
   usethis:::use_dependency("covr", "Suggests")
-  new <- usethis::use_template("codecov.yml", ignore = TRUE)
-    if (!new) {
-      return(invisible(FALSE))
-    }
+
+  if (! usethis::use_template("codecov.yml", ignore = TRUE)) {
+    return(invisible(FALSE))
+  }
 }
