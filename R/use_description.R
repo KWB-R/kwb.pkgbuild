d21944f009857f7bbfb11377c59a63b115566922
hsonne <hauke.sonnenberg@gmx.de>
2019-03-03 00:28:42 +0100

    Clean use_description.R
    - do not use multiline strings
    - simplify name: firstname_surname <- full_name
    - reindent

--- R/use_description.R
+++ R/use_description.R
@@ -1,99 +1,105 @@
-#' use_description with KWB styling
+# use_description --------------------------------------------------------------
+
+#' Use DESCRIPTION with KWB styling
 #'
-#' @param author author information in list format (default:
-#' list(name = "Michael Rustler", orcid = "0000-0003-0647-7726",
-#' url = "https://mrustl.de"))
-#' @param pkg package description in list format (default:\cr
-#' pkg = list(name = "kwb.umberto",\cr
-#' title = "R package supporting UMBERTO LCA at KWB",\cr
-#' desc = "Helper functions for data aggregation and visualisation of\cr
-#' UMBERTO (https://www.ifu.com/umberto/) model output"))
+#' @param author author information in list format (default: list(name =
+#'   "Michael Rustler", orcid = "0000-0003-0647-7726", url =
+#'   "https://mrustl.de"))
+#' @param pkg package description in list format (default:\cr pkg = list(name =
+#'   "kwb.umberto",\cr title = "R package supporting UMBERTO LCA at KWB",\cr
+#'   desc = "Helper functions for data aggregation and visualisation of\cr
+#'   UMBERTO (https://www.ifu.com/umberto/) model output"))
 #' @param version user defined version number (e.g. 0.1.0) or default version
-#' number 0.0.0.9000 in case (version = NULL) of first release
+#'   number 0.0.0.9000 in case (version = NULL) of first release
 #' @param license license (default: "MIT + file LICENSE")
-#' @param copyright_holder_name name of copyright holder
-#' (default: "Kompetenzzentrum Wasser Berlin gGmbH (KWB)")
-#' @param funder funder/funding agency of R package (default: NULL), e.g. project
-#' name (e.g. AQUANES)
+#' @param copyright_holder_name name of copyright holder (default:
+#'   "Kompetenzzentrum Wasser Berlin gGmbH (KWB)")
+#' @param funder funder/funding agency of R package (default: NULL), e.g.
+#'   project name (e.g. AQUANES)
 #' @return writes DESCRIPTION file using usethis::use_description() with KWB
-#' style
+#'   style
 #' @importFrom usethis use_description
 #' @importFrom stringr str_split
 #' @importFrom tools toTitleCase
 #' @export
-use_description <- function(author = list(name = "Michael Rustler",
- orcid = "0000-0003-0647-7726",
- url = "https://mrustl.de"),
- pkg = list(name = "kwb.umberto",
- title = "R package supporting UMBERTO LCA at KWB",
- desc = paste0("Helper functions for data aggregation and visualisation",
- " of UMBERTO (https://www.ifu.com/umberto/) model output.")),
- version = NULL,
- license = "MIT + file LICENSE",
-copyright_holder_name = "Kompetenzzentrum Wasser Berlin gGmbH (KWB)",
-                     funder = NULL) {
-
-  if(is.null(version)) {
+use_description <- function(
+  author = list(
+    name = "Michael Rustler",
+    orcid = "0000-0003-0647-7726",
+    url = "https://mrustl.de"
+  ),
+  pkg = list(
+    name = "kwb.umberto",
+    title = "R package supporting UMBERTO LCA at KWB",
+    desc = paste0(
+      "Helper functions for data aggregation and visualisation",
+      " of UMBERTO (https://www.ifu.com/umberto/) model output."
+    )
+  ),
+  version = NULL,
+  license = "MIT + file LICENSE",
+  copyright_holder_name = "Kompetenzzentrum Wasser Berlin gGmbH (KWB)",
+  funder = NULL
+)
+{
+  if (is.null(version)) {
     version <- "0.0.0.9000"
-    cat(sprintf("No version specified. Using default initial version %s\n", version))
+    cat(sprintf(
+      "No version specified. Using default initial version %s\n", version
+    ))
   }
 
-pkg$title <- tools::toTitleCase(pkg$title)
+  pkg$title <- tools::toTitleCase(pkg$title)
 
-firstname_surname <- stringr::str_split(string = author$name,pattern = "\\s+")[[1]]
-author_name <- firstname_surname[1]
-author_surname <- firstname_surname[2]
+  full_name <- stringr::str_split(string = author$name,pattern = "\\s+")[[1]]
+  author_name <- full_name[1]
+  author_surname <- full_name[2]
 
+  author_email <- sprintf(
+    '%s.%s@kompetenz-wasser.de', tolower(author_name), tolower(author_surname)
+  )
 
-author_email <- sprintf('%s.%s@kompetenz-wasser.de',
-                        tolower(author_name),
-                        tolower(author_surname))
+  author_comment <- if (is.null(author$orcid)) {
+    "NULL"
+  } else {
+    sprintf('c(ORCID = "%s")', author$orcid)
+  }
 
-if(is.null(author$orcid)) {
-  author_comment <- "NULL"
-} else {
-  author_comment <- sprintf('c(ORCID = "%s")', author$orcid)
+  desc_author <- sprintf(
+    'person("%s", "%s", , "%s",\n      role = c("aut", "cre"), comment = %s)',
+    author_name, author_surname, author_email, author_comment
+  )
+
+  desc_copyright_holder <- sprintf(
+    'person("%s", role = c("cph"))', copyright_holder_name
+  )
+
+  authors_meta <- sprintf('%s,\n      %s', desc_author, desc_copyright_holder)
+
+  if (! is.null(funder)) {
+    desc_funder <- sprintf('person("%s", role = c("fnd"))', funder)
+    authors_meta <-sprintf("%s,\n      %s", authors_meta, desc_funder)
+  }
+
+  authors_meta <- sprintf("c(%s)", authors_meta)
+
+  url <- "https://github.com/KWB-R"
+
+  options(
+    usethis.name = author$name,
+    usethis.description = list(
+      Package = pkg$name,
+      Title = pkg$title,
+      Description = pkg$desc,
+      `Authors@R` = authors_meta,
+      License = license,
+      Version = version,
+      URL = sprintf("%s/%s", url, pkg$name),
+      BugReports = sprintf("%s/%s/issues", url, pkg$name)
+    )
+  )
+
+  kwb.utils::catAndRun("Creating KWB DESCRIPTION file", {
+    usethis::use_description(fields = NULL)
+  })
 }
-
-desc_author <- sprintf('person("%s", "%s", , "%s",
-           role = c("aut", "cre"), comment = %s)',
-                  author_name,
-                  author_surname,
-                  author_email,
-                  author_comment)
-
-desc_copyright_holder <- sprintf('person("%s", role = c("cph"))', copyright_holder_name)
-
-
-authors_meta <- sprintf('%s,
-           %s', desc_author, desc_copyright_holder)
-
-if (!is.null(funder)) {
-  desc_funder <- sprintf('person("%s", role = c("fnd"))',funder)
-  authors_meta <-sprintf("%s,
-            %s", authors_meta, desc_funder)
-}
-
-authors_meta <- sprintf("c(%s)", authors_meta)
-
-
-options(
-  usethis.name = author$name,
-  usethis.description = list(
-  Package = pkg$name,
-  Title = pkg$title,
-  Description = pkg$desc,
-  `Authors@R` = authors_meta,
-  License = license,
-  Version = version,
-  URL = sprintf("https://github.com/KWB-R/%s", pkg$name),
-  BugReports = sprintf("https://github.com/KWB-R/%s/issues", pkg$name)
-)
-)
-
-cat("Creating KWB DESCRIPTION file....\n")
-usethis::use_description(fields = NULL)
-cat("Creating DESCRIPTION file....done.\n")
-
-}
-
