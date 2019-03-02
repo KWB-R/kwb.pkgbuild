2339009144602df13dca701142cdb9176f116717
hsonne <hauke.sonnenberg@gmx.de>
2019-03-03 00:55:54 +0100

    Clean use_mit_license.R
    - do not use arg names in file_copy. It is clear but
      shorter
    - reindent

--- R/use_mit_license.R
+++ R/use_mit_license.R
@@ -11,26 +11,30 @@
 #' @importFrom fs file_copy
 #' @export
 use_mit_license <- function(
-  copyright_holder = list(name ="Kompetenzzentrum Wasser Berlin gGmbH (KWB)",
-                          start_year = NULL)) {
+  copyright_holder = list(
+    name ="Kompetenzzentrum Wasser Berlin gGmbH (KWB)",
+    start_year = NULL
+  )
+)
+{
+  cat("Creating KWB MIT LICENSE file....\n")
+  usethis::use_mit_license(name = copyright_holder$name)
 
-cat("Creating KWB MIT LICENSE file....\n")
-usethis::use_mit_license(name = copyright_holder$name)
+  if (! is.null(copyright_holder$start_year)) {
 
-if (!is.null(copyright_holder$start_year)) {
+    lic_txt <- readLines("LICENSE.md")
+    index <- stringr::str_detect(lic_txt,pattern = "^Copyright\\s+\\(c\\)")
 
-  lic_txt <- readLines("LICENSE.md")
-  index <- stringr::str_detect(lic_txt,pattern = "^Copyright\\s+\\(c\\)")
+    lic_txt[index] <- sprintf(
+      "Copyright (c) %s-%s %s",
+      as.character(copyright_holder$start_year),
+      format(Sys.Date(), format = "%Y"),
+      copyright_holder$name
+    )
 
-  cpy_holder <- sprintf("Copyright (c) %s-%s %s",
-                      as.character(copyright_holder$start_year),
-                      format(Sys.Date(), format = "%Y"),
-                      copyright_holder$name)
+    writeLines(lic_txt, "LICENSE.md")
+  }
 
-  lic_txt[index] <- cpy_holder
-  writeLines(lic_txt, "LICENSE.md")
- }
-
-fs::file_copy(path = "LICENSE.md", new_path = "LICENSE",overwrite = TRUE)
-cat("Creating MIT LICENSE file....done.\n")
+  fs::file_copy("LICENSE.md", "LICENSE", overwrite = TRUE)
+  cat("Creating MIT LICENSE file....done.\n")
 }
