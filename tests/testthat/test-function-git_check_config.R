d22a5a1e3aa5a11d4725ad40a5d0873c83185422
mrustl <michael.rustler@kompetenz-wasser.de>
2019-03-14 22:29:50 +0100

    appveyor: execute test on windows (to help #46)

--- tests/testthat/test-function-git_check_config.R
+++ tests/testthat/test-function-git_check_config.R
@@ -1,5 +1,5 @@
 test_that("git_setup_user() works", {
-  skip_on_appveyor()
+
   expect_error(
     kwb.pkgbuild:::git_setup_user("testuser", "test.user@doesnotmatter.eu")
   )
--- tests/testthat/test-function-git_setup_user.R
+++ tests/testthat/test-function-git_setup_user.R
@@ -1,5 +1,5 @@
 test_that("git_check_config() works", {
-  skip_on_appveyor()
+
   expect_error(
     kwb.pkgbuild:::git_check_config()
   )
