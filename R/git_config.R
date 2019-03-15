9fbf7265dadc9ef563809bd0df9c8d9ae64abe78
mrustl <michael.rustler@kompetenz-wasser.de>
2019-03-15 14:50:31 +0100

    use git_exe on windows (closes #47)

--- R/git_config.R
+++ R/git_config.R
@@ -5,28 +5,35 @@
 #' @param key key (default: "--list")
 #' @param value value (default: "")
 #' @param scope scope (default: c("local", "global"))
+#' @param git_exe path to git executable on Windows (default: path_to_git())
 #' @noRd
 #' @keywords internal
-git_config <- function(key = "--list", value = "", scope = c("local", "global"))
+git_config <- function(key = "--list", value = "", scope = c("local", "global"),
+                       git_exe = path_to_git())
 {
-  sprintf("git config --%s %s %s", scope, key, value)
+
+  git <- git_check_if_windows(git_exe)
+  sprintf('%s config --%s %s %s',
+          git_check_if_windows(git_exe),
+          scope, key, value)
 }
 
 #' Git Check Configuration
 #'
+#' @param git_exe path to git executable on Windows (default: path_to_git())
 #' @return prints 'global' and 'local' Git configuration
 #' @export
 #' @examples
 #' \dontrun{
 #' git_check_config()
 #' }
-git_check_config <- function()
+git_check_config <- function( git_exe = path_to_git())
 {
   message("Checking 'global' Git(Hub) config:")
-  shell(git_config(scope = "global"))
+  shell(git_config(scope = "global", git_exe = git_exe))
 
   message("Checking 'local' Git(Hub) config:")
-  shell(git_config(scope = "local"))
+  shell(git_config(scope = "local", git_exe = git_exe))
 }
 
 
@@ -36,6 +43,7 @@ git_check_config <- function()
 #' @param github_email email should be identical to email you registered with at
 #' GitHub (e.g. your.name(at)kompetenz-wasser.de)
 #' @param scope scope (default: c("local", "global"))
+#' @param git_exe path to git executable on Windows (default: path_to_git())
 #' @return runs "git" to setup the Git(Hub) user
 #' @export
 #' @importFrom kwb.utils catAndRun
@@ -46,12 +54,13 @@ git_check_config <- function()
 #' @seealso <https://support.rstudio.com/hc/en-us/community/posts/115001143667-Author-Change-with-Git-in-RStudio>
 git_setup_user <- function(github_username,
                            github_email,
-                           scope = c("local", "global"))
+                           scope = c("local", "global"),
+                           git_exe = path_to_git())
 {
 
 
-cmds <- c(git_config(key = "user.name", value = github_username, scope),
-          git_config(key = "user.email", value = github_email, scope)
+cmds <- c(git_config(key = "user.name", value = github_username, scope, git_exe),
+          git_config(key = "user.email", value = github_email, scope, git_exe)
           )
 
 kwb.utils::catAndRun("Setup Git(Hub) user",
